ebcb2c2fb10b9a06835eeb77d742c016bec694ce
macnum <w.damilola@yahoo.com>
2023-08-20 14:56:25 +0100

    Update script for ui

--- main.js
+++ main.js
@@ -1,61 +1,87 @@
 const weapon = ["rock","paper","scissors"];
-let count = 0;
-// when a user makes a choice
-const userChoice =prompt("Enter rock, paper.scissors").toLocaleLowerCase();
-
+const btn = document.querySelectorAll("button")
+const resultDisplay = document.querySelector(".result");
+const scoreDisplay = document.querySelector(".score")
+const userDisplay = document.querySelector(".user");
+const computerDisplay = document.querySelector(".computer");
+let computerChoice ="";
+let userChoice = "";
+let comScore = 0;
+let userScore = 0;
 // Computer makes a choice randomly
 const getRandomNumber = () => Math.floor(Math.random() * weapon.length);
 
 const getComputerChoice =() => weapon[getRandomNumber()];
-let computerChoice = getComputerChoice()
+
+// when a user makes a choice
+
+
+
+btn.forEach(x => {
+	x.addEventListener('click', (e)=> {
+	userChoice = e.target.dataset.value;
+	computerChoice = getComputerChoice();
+
+	let resultOfComparison = compareChoice();
+	userDisplay.textContent = `User Choice: ${userChoice} `;
+	computerDisplay.textContent = `Computer Choice: ${computerChoice} `;
+	resultDisplay.textContent = `Result: ${resultOfComparison} `;
+	scoreDisplay.textContent = `Score: comp*(${comScore}) || user(${userScore})`;
+	checkWinner();
+
+	})
+});
+
+function checkWinner() {
+// body...
+	if(userScore < comScore && comScore == 5) {
+		scoreDisplay.textContent = "Computer Wins!";
+		comScore =0;
+		userScore = 0;
+	}
+	else if(comScore <userScore  && userScore == 5) {
+		scoreDisplay.textContent = "Congrats! You Win"
+		comScore =0;
+		userScore = 0;
+	}
+	
+}
+
 
 const compareChoice = () => {
 	// When choice are the same
 	if(userChoice === computerChoice) {
-		result = "A Draw"
+		result = "A Draw";
 	}
 
 	//Computer wins
 	else if(computerChoice === 'rock' && userChoice ==='scissors') {
+		comScore++;
 		result = "You Lose! Rock beats scissors"
 	}
 	else if(computerChoice === 'paper' && userChoice ==='rock') {
+		comScore++;
 		result = "You Lose! Paper beats Rock"
 	}
 	else if(computerChoice === 'scissors' && userChoice ==='paper') {
+		comScore++;
 		result = "You Lose! Scissors beats Paper"
 	}
 	//user wins
 	else if(userChoice === 'rock' && computerChoice ==='scissors') {
+		userScore++;
 		result = "You Win! Rock beats scissors";
 	}
 	else if(userChoice === 'paper' && computerChoice =='rock') {
+		userScore++;
 		result = "You Win! Paper beats Rock";
 	}
 	else if(userChoice === 'scissors' && computerChoice ==='paper') {
-		result = "You Win! Scissors beats Paper"	
+		userScore++;
+		result = "You Win! Scissors beats Paper";
 	}
 	
 	return result;
 }
 
 
-function playGround(userChoice, computerChoice) {
-	return (
-		"User Choice: "+userChoice+"\nComputer Choice: " + computerChoice+"\n"+
-		compareChoice()
-)
-}
-
-function game() {
-	let playedArr =[];
-	for(let i =0;i< 5;i++) {
-		let userSelect =userChoice;
-		computerChoice = getComputerChoice()
-
-		playedArr.push(playGround(userSelect,computerChoice));
-	}
-	return playedArr.toString().replace(/,/g,'\n\n');
-}
-
-console.log(game())
